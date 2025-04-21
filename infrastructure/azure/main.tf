53dbf3f8c4cf484123649851d411d22ffeb026b6
Emil Sandnabba <emil@sandnabba.se>
2025-04-21 13:39:32 +0200

    Ignore delegations (used by App Services)

--- infrastructure/azure/main.tf
+++ infrastructure/azure/main.tf
@@ -26,5 +26,9 @@ resource "azurerm_subnet" "subnet" {
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = var.subnet_address_prefix
+
+  lifecycle {
+    ignore_changes = [delegation]
+  }
 }
 
